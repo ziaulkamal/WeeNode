c6b66de78e5e704823703c3612bfee90b0dbd08c
Ziaul Kamal <ziaulkamal1109@gmail.com>
2022-12-23 00:04:39 +0700

    main

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+// Setup static File Location
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+    res.render('layout')
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
